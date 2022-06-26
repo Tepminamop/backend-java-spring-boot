d5a887af6c8a449d00c50efba2b87f7ece1b01d0
Tepminamop <fanbeyblade2001@gmail.com>
2022-06-27 02:50:00 +0300

    added .pid file

--- src/main/java/com/backend/backend/BackendApplication.java
+++ src/main/java/com/backend/backend/BackendApplication.java
@@ -2,12 +2,15 @@ package com.backend.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication springApplication = new SpringApplication(BackendApplication.class);
+		springApplication.addListeners(new ApplicationPidFileWriter());
+		springApplication.run(args);
 	}
 
 }
